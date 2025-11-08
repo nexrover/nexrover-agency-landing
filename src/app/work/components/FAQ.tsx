f76810dd54c519aa413ee891c721e1b6db3382e3
MD RAHATUL ISLAM <rahathridoyd2x@gmail.com>
2025-11-08 07:08:23 +0600

    Fix: FAQ issue

--- src/app/work/components/FAQ.tsx
+++ src/app/work/components/FAQ.tsx
@@ -1,8 +1,12 @@
+"use client";
 import SectionHeading from "@/components/features/SectionHeading";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const FAQ = () => {
-  const faqItems = [
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  const leftFAQ = [
     {
       id: 1,
       question: "What services do you offer?",
@@ -11,88 +15,134 @@ const FAQ = () => {
     },
     {
       id: 2,
-      question: "What services do you offer?",
+      question: "How long does it take to complete a project?",
       answer:
-        "We offer a wide range of services including web development, mobile app development, UI/UX design, and digital marketing.",
+        "Project timelines vary depending on complexity, but we always ensure transparent communication and timely delivery.",
     },
     {
       id: 3,
-      question: "What services do you offer?",
+      question: "Do you provide ongoing support after launch?",
       answer:
-        "We offer a wide range of services including web development, mobile app development, UI/UX design, and digital marketing.",
+        "Yes! We offer maintenance, updates, and technical support after your product goes live to ensure everything runs smoothly.",
     },
+  ];
+  const rightFAQ = [
     {
       id: 4,
-      question: "What services do you offer?",
+      question: "What services offer?",
       answer:
         "We offer a wide range of services including web development, mobile app development, UI/UX design, and digital marketing.",
     },
     {
       id: 5,
-      question: "What services do you offer?",
+      question: "How long does it take to complete a project?",
       answer:
-        "We offer a wide range of services including web development, mobile app development, UI/UX design, and digital marketing.",
+        "Project timelines vary depending on complexity, but we always ensure transparent communication and timely delivery.",
     },
     {
       id: 6,
-      question: "What services do you offer?",
+      question: "Do you provide ongoing support after launch?",
       answer:
-        "We offer a wide range of services including web development, mobile app development, UI/UX design, and digital marketing.",
+        "Yes! We offer maintenance, updates, and technical support after your product goes live to ensure everything runs smoothly.",
     },
   ];
+
   return (
     <section className="section-padding">
       <div className="container mx-auto">
-        {/* Section heading */}
         <SectionHeading
           head="Frequently Asked Questions"
           colorHead="primary-text"
           textAlign="center"
         />
-
         {/* FAQ wrapper */}
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-10  mt-6 md:mt-8 lg:mt-10 ">
-          {faqItems.map((item) => (
-            <div key={item.id}>
-              {/* Accordion Item 1 */}
-              <div className="bg-secondary-bg rounded-md overflow-hidden border border-border max-h-[200px] ">
-                <input
-                  type="checkbox"
-                  id={item.id.toString()}
-                  className="peer hidden "
-                />
-
+        <div className="flex flex-col lg:flex-row gap-0 lg:gap-8">
+          {/* left FAQ */}
+          <div className="flex flex-col gap-3 mt-8">
+            {leftFAQ.map((item) => (
+              <div
+                key={item.id}
+                className="bg-secondary-bg rounded-md border border-border overflow-hidden"
+              >
                 {/* Header */}
-                <label
-                  htmlFor={item.id.toString()}
-                  className="flex justify-between p-5 cursor-pointer select-none "
+                <button
+                  onClick={() =>
+                    setActiveId(activeId === item.id ? null : item.id)
+                  }
+                  className="flex justify-between items-center w-full text-left p-3 lg:p-5 cursor-pointer select-none"
                 >
-                  <span className="com-text font-medium ">{item.question}</span>
+                  <span className="com-text font-medium">{item.question}</span>
 
                   {/* Icons */}
-                  <div className="relative w-5 h-5">
-                    {/* Plus Icon */}
-                    <div className="absolute inset-0 flex items-center justify-center transition-transform duration-300 peer-checked:rotate-90 opacity-100 peer-checked:opacity-0">
-                      <div className="w-5 h-0.5 bg-secondary-text rounded-full"></div>
-                      <div className="w-0.5 h-5 bg-secondary-text rounded-full absolute"></div>
-                    </div>
-
-                    {/* Minus Icon */}
-                    <div className="absolute inset-0 flex items-center justify-center transition-all duration-300 opacity-0 peer-checked:opacity-100">
-                      <div className="w-5 h-0.5 bg-secondary-text rounded-full"></div>
-                    </div>
+                  <div className="relative w-5 h-5 flex items-center justify-center">
+                    <Image
+                      src="/icons/down-arrow.svg"
+                      alt="arrow-icon"
+                      width={6}
+                      height={6}
+                      className={`w-5 h-5 transition ${
+                        activeId === item.id ? "rotate-180" : "rotate-0"
+                      }`}
+                    />
                   </div>
-                </label>
+                </button>
 
                 {/* Content */}
-                <div className="max-h-0 overflow-hidden transition-all duration-300 peer-checked:max-h-96">
-                  <div className="p-5  text-secondary-text border-t border-border">
-                    <p>{item.answer}</p>
+                <div
+                  className={`transition-all duration-300 overflow-hidden ${
+                    activeId === item.id ? "max-h-[150px]" : "max-h-0"
+                  }`}
+                >
+                  <div className="p-5 border-t border-border text-secondary-text">
+                    {item.answer}
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          {/* right FAQ */}
+          <div className="flex flex-col gap-3 mt-3 lg:mt-8">
+            {rightFAQ.map((item) => (
+              <div
+                key={item.id}
+                className="bg-secondary-bg rounded-md border border-border overflow-hidden"
+              >
+                {/* Header */}
+                <button
+                  onClick={() =>
+                    setActiveId(activeId === item.id ? null : item.id)
+                  }
+                  className="flex justify-between items-center w-full text-left p-3 lg:p-5 cursor-pointer select-none"
+                >
+                  <span className="com-text font-medium">{item.question}</span>
+
+                  {/* Icons */}
+                  <div className="relative w-5 h-5 flex items-center justify-center">
+                    <Image
+                      src="/icons/down-arrow.svg"
+                      alt="arrow-icon"
+                      width={6}
+                      height={6}
+                      className={`w-5 h-5 transition ${
+                        activeId === item.id ? "rotate-180" : "rotate-0"
+                      }`}
+                    />
+                  </div>
+                </button>
+
+                {/* Content */}
+                <div
+                  className={`transition-all duration-300 overflow-hidden ${
+                    activeId === item.id ? "max-h-[150px]" : "max-h-0"
+                  }`}
+                >
+                  <div className="p-5 border-t border-border text-secondary-text">
+                    {item.answer}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
